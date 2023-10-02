92190e99fe06f1cf1744fcf3b8456ae5075731a3
Benjamin Nduagubu <nduagubu@gmail.com>
2023-10-02 18:18:42 -0400

    uncommented credentials

--- module-call/main.tf
+++ module-call/main.tf
@@ -1,7 +1,7 @@
 module "s3" {
   source = "git::https://github.com/BensGits/tickets.git//s3-module"
-#   access_key = var.access_key
-#   secret_key = var.secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 # variable "access_key" {
