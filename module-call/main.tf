12048ed54b7eca2ad28042f68be61925fa50cda1
Benjamin Nduagubu <nduagubu@gmail.com>
2023-10-02 15:18:21 -0400

    try

--- module-call/main.tf
+++ module-call/main.tf
@@ -1,5 +1,15 @@
 module "s3" {
   source = "git::https://github.com/BensGits/tickets.git//s3-module"
-  access_key = var.access_key
-  secret_key = var.secret_key
+#   access_key = var.access_key
+#   secret_key = var.secret_key
 }
+
+# variable "access_key" {
+#   type        = string
+#   description = "access"
+# }
+
+# variable "secret_key" {
+#   type        = string
+#   description = "password"
+#}
